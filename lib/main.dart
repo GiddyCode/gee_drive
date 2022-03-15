0873c85b958ee51769364c07765d0da4c54d84ae
GiddyCode <ogbonnagideon1994@gmail.com>
2022-03-15 17:30:02 +0100

    started the ui for login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gee_drive/AllScreens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,35 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child:  Container(
-          child: const Text('This is just a container'),
-        )
-      ),
+      home: const MainScreen(),
     );
   }
 }
